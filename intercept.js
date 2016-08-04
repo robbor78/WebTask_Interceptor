694a94ff3ac110fbfc80d1dafa9d2b1ba6ae4a72
name <email>
2016-08-05 01:03:43 +0200

    Sample webtask, that demonstrates how to redirect.

---
+++ intercept.js
@@ -0,0 +1,45 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var insertDocument = function(db, req, callback) {
+    var bv = db          .collection('blogvisitors');
+
+    bv.insertOne({
+        date: Date.now(),
+        useragent: req.headers['user-agent'],
+        method: req.method},
+                 function(err,result) {
+                     console.log('data inserted.');
+                     callback();
+
+                 });
+};
+
+module.exports = function(context, req, res) {
+    console.log('incoming request... ');
+    console.log(req);
+
+    MongoClient.connect(context.data.MONGO_URL, function (err, db) {
+        if(err) return done(err);
+
+        console.log('saving to db...');
+
+        insertDocument(db, req, function() {
+            console.log('closing db connection');
+            db.close();
+
+            console.log('constructing response...');
+            res.writeHead(301,
+                          {
+                              'Location': 'https://github.com/robbor78',
+                              'Content-Type': 'text/html'
+                          });
+            res.end('<html><head><title>Redirecting...</title></head><body><h1>Redirecting</h1><p>Redirecting to <a href="https://github.com/robbor78">https://github.com/robbor78</a>.</p></body></html>');
+
+            console.log('redirecting...');
+        });
+
+    });
+
+
+};
+
